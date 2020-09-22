57c16a582034368116089ed5193b23dbe269b509
Joao Pedro <joao.ricardo88@gmail.com>
2020-09-22 22:24:17 +0100

    first commit

---
+++ src/components/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function First () {
+    const msg = 'Learning React'
+
+    return (
+        <div>
+             <h2>Hello World!</h2>
+             <p>{ msg }</p>
+        </div> 
+    )
+  }
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import First from './components/First'
+
+
+ReactDOM.render(
+    <div> 
+        <First></First>
+    </div>,
+    document.getElementById('root')
+)
