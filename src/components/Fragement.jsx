c5522fbb95e526833d353f6332beeeda8e28e1c4
Joao Pedro <joao.ricardo88@gmail.com>
2020-09-28 14:45:48 +0100

    small changes in my code

---
+++ src/components/Fragement.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fragment (params) {
+    return (
+        <>
+            <h2>New Text</h2>
+            <p>Here is new text</p>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 import First from './components/First'
 import Parameter from './components/Parameter'
+import Fragement from './components/Fragement'
+
 
 
 ReactDOM.render(
@@ -15,6 +17,7 @@ ReactDOM.render(
         <Parameter title = 'Game' 
         subtitle = 'Trivial' 
         rate ={3.5} />
+        <Fragement />
     </div>,
     document.getElementById('root')
 )
